879af092368b7c2bf5bc5e2e9941e7343a8718a5
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2025-03-17 19:29:41 +0100

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -14,7 +14,7 @@ cat <<EOF > /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
     "mode":"tcp_and_udp",
     "server_port":8388,
     "local_port":1080,
-    "password":"558955",
+    "password":"135890",
     "timeout":60,
     "fast_open":true,
     "reuse_port": true,
